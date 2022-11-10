8657701d1b151fd30b33ee5c97b09a84fcf1ea9d
Alexander Davis <alexander.davis.098@gmail.com>
2022-11-10 09:55:29 -0300

    Update: using swagger-jsdoc in the project

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const swagguerUi = require('swagger-ui-express') 
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+const swaggerConfig = require('./docs/swaggerConfig')
 
 const app = express()
 const PORT = process.env.PORT || 9000
@@ -11,14 +13,25 @@ const PORT = process.env.PORT || 9000
 app.use(cors())
 app.use(bodyParser.json())
 
-const docs = require('./docs/swagger')
 const auth = require('../src/routes/Auth')
 const user = require('../src/routes/User')
 
-app.use('/api/v1/docs', swagguerUi.serve, swagguerUi.setup(docs))
+// Swagger Configs
+if (process.env.NODE_ENV !== 'test') {
+    const swaggerSpec = (swaggerJsDoc(swaggerConfig))
+    app.get('/api/v1/swagger.json',  (req, res)=> {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(swaggerSpec);
+    });
+    
+    app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
+
+// Routes
 app.use('/api/v1/auth', auth)
 app.use('/api/v1/users', user)
 
+// Database
 mongoose
     .connect('mongodb://database:27017/jwt-node-mongo-docker', {
         useNewUrlParser: true
