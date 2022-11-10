048f24c62807e66d118d7c917a8e73837db9e883
Alexander Davis <alexander.davis.098@gmail.com>
2022-11-09 23:20:38 -0300

    Fix: Fixing user controller

--- src/controllers/User/index.js
+++ src/controllers/User/index.js
@@ -1,17 +1,22 @@
 const User = require("../../models/User")
 
 exports.users = async (req, res) => {
-    await User.find()
-        .then(users => {
-            res.json(users)
-        })
-        .catch(error => res.status(500).json(error))
+    try {
+        const users = await User.find()
+        return res.status(200).json({ users: users })
+    } catch (error) {
+        return res.status(500).json({ message: 'Could not load the user list. An internal server error has occurred.' })
+    }
 }
 
 exports.searchUserByEmail = async (req, res) => {
-    await User.findOne({ email: req.params.email })
-        .then(user => {
-            res.json(user)
-        })
-        .catch(error => res.status(500).json(error))
+    try {
+        const user = await User.findOne({ email: req.params.email })
+        if (!user)
+            return res.status(404).json({ message: 'This user does not exist.' })
+
+        return res.status(200).json({ user: user })
+    } catch (error) {
+        return res.status(500).json({ message: 'Error finding user. An internal server error has occurred.' })
+    }
 }
