6735e6c2927c24bc93ac726bca8cc1c7dfe96958
Alexander Davis <alexander.davis.098@gmail.com>
2023-08-09 10:44:52 -0300

    test: added new tests for user listing and authentication

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -10,15 +10,16 @@ beforeEach(async () => {
     jest.clearAllMocks()
 })
 
-/* Dropping the database and closing connection after each test. */
-afterEach(async () => {
-    await mongoose.connection.dropDatabase();
+/* Dropping the database and closing the connection after all tests. */
+afterAll(async () => {
+    await mongoose.connection.dropDatabase()
     await mongoose.connection.close()
 })
 
 const check = jest.fn(async (user) => {
     const existingUser = await User.findOne({ email: user.email })
     if (existingUser) throw new Error('This user already exists!')
+    if (!existingUser) throw new Error('This user does not exist.')
 })
 
 describe('Create User', () => {
@@ -35,7 +36,6 @@ describe('Create User', () => {
     })
 
     it('Should not be able to create an existing user', async () => {
-
         const ExistingUser = {
             name: 'Existing User',
             email: 'existing.user@gmail.com',
@@ -49,5 +49,103 @@ describe('Create User', () => {
 
         expect(response.status).toBe(400)
         await expect(check(ExistingUser)).rejects.toEqual(new Error('This user already exists!'))
+    })
+})
+
+describe('List Users', () => {
+    it('Should be able to return all users', async () => {
+        const response = await request(app).get('/api/v1/users')
+
+        expect(response.status).toBe(200)
+        expect(response.body.users.length).toBeGreaterThan(0)
+    })
+
+    it('Should be able to return a user by email', async () => {
+        const response = await request(app).get('/api/v1/users/alexander.davis@gmail.com')
+
+        expect(response.status).toBe(200)
+        expect(response.body.user.name).toBe('Alexander Davis')
+    })
+
+    it('Should not be able to find a user that doesn\u0027t exist', async () => {
+        const UserDoesNotExist = {
+            email: 'user.does.not.exist@gmail.com'
+        }
+
+        const response = await request(app).get(`/api/v1/users/${UserDoesNotExist.email}`)
+
+        expect(response.status).toBe(404)
+        await expect(check(UserDoesNotExist)).rejects.toEqual(new Error('This user does not exist.'))
+    })
+})
+
+describe('User Authentication Steps', () => {
+    let token = ''
+    let refreshToken = ''
+
+    beforeAll(async () => {
+        const response = await request(app).post('/api/v1/auth/login').send({
+            email: 'alexander.davis@gmail.com',
+            password: '12345678'
+        })
+
+        /* Getting the authentication token. */
+        token = response.body.token
+        refreshToken = response.body.refreshToken
+    })
+
+    it('Should be able to log in', async () => {
+        const response = await request(app).post('/api/v1/auth/login').send({
+            email: 'alexander.davis@gmail.com',
+            password: '12345678'
+        })
+
+        expect(response.status).toBe(200)
+        expect(response.body.message).toBe('Successfully authenticated!')
+        expect(response.body).toHaveProperty('token')
+        expect(response.body).toHaveProperty('refreshToken')
+    })
+
+    it('Should not be able to log in', async () => {
+        const User = {
+            email: 'alexander.davis2@gmail.com',
+            password: '12345678'
+        }
+
+        const response = await request(app).post('/api/v1/auth/login').send(User)
+
+        expect(response.status).toBe(404)
+        await expect(check(User)).rejects.toEqual(new Error('This user does not exist.'))
+    })
+
+    it('Should be able to perform user authentication', async () => {
+        const response = await request(app).get('/api/v1/auth').set('Authorization', `Bearer ${token}`)
+
+        expect(response.status).toBe(200)
+        expect(response.body.message).toBe('Welcome to the API')
+    })
+
+    it('Should not be able to perform user authentication', async () => {
+        const response = await request(app).get('/api/v1/auth').set('Authorization', 'Bearer sajd348@#435901X|ZSAS1')
+
+        expect(response.status).toBe(403)
+    })
+
+    it('Should be able to generate a new access token', async () => {
+        const response = await request(app).post('/api/v1/auth/refresh').set('Accept', 'application/json').send({
+            refreshToken: refreshToken
+        })
+
+        expect(response.status).toBe(200)
+        expect(response.body.message).toBe('Token updated successfully!')
+        expect(response.body).toHaveProperty('token')
+    })
+
+    it('Should not be able to generate a new access token', async () => {
+        const response = await request(app).post('/api/v1/auth/refresh').set('Accept', 'application/json').send({
+            refreshToken: 'sajd348@#435901X|ZSAS1'
+        })
+
+        expect(response.status).toBe(403)
     })
 })
